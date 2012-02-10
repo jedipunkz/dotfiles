33a2d6194e3d0231c2b6bced0048fc412135d7c0
Tomokazu Hirai <tomokazu.hirai@gmail.com>
2012-02-10 16:05:54 +0900

    mod for some colors

--- .mew-theme.el
+++ .mew-theme.el
@@ -1,9 +1,7 @@
 ;;;
-;;; Copy this contents to "~/.mew.el".
-;;;
+;;; Copy this contents to "~/.mew.el".el;;;
 ;;; Or, copy this file to a file, say "~/.mew-theme.el" and
-;;; put the following to "~/.mew.el".
-;;;	(setq mew-theme-file "~/.mew-theme.el")
+;;; put the following to "~/.mew.el".el;;;(setq mew-theme-file "~/.mew-theme.el")
 
 ;;;; COPY FROM HERE
 
@@ -12,36 +10,36 @@
 ;;; Highlight header
 ;;;
 
-(mew-setface-bold header-subject
-  :tty "yellow" :light "Firebrick" :dark "OrangeRed")
+(mew-setface header-subject
+  :tty "red" :light "Firebrick" :dark "OrangeRed")
 
-(mew-setface-bold header-from
-  :tty "#332200" :light "DarkOrange4" :dark "Gold")
+(mew-setface header-from
+  :tty "yellow" :light "DarkOrange4" :dark "Gold")
 
-(mew-setface-bold header-date
-  :tty "cyan" :light "ForestGreen" :dark "LimeGreen")
-
-(mew-setface-bold header-to
-  :tty "magenta" :light "DarkViolet" :dark "violet")
-
-(mew-setface-bold header-key
+(mew-setface header-date
   :tty "green" :light "ForestGreen" :dark "LimeGreen")
 
-(mew-setface-bold header-private)
+(mew-setface header-to
+  :tty "magenta" :light "DarkViolet" :dark "violet")
 
-(mew-setface-bold header-important
-  :tty "blue" :light "MediumBlue" :dark "SkyBlue")
+(mew-setface header-key
+  :tty "green" :light "ForestGreen" :dark "LimeGreen")
 
-(mew-setface-bold header-marginal
+(mew-setface header-private)
+
+(mew-setface header-important
+  :tty "cyan" :light "MediumBlue" :dark "SkyBlue")
+
+(mew-setface header-marginal
   :light "gray50" :dark "gray50")
 
-(mew-setface-bold header-warning
+(mew-setface header-warning
   :tty "red" :light "red" :dark "red")
 
-(mew-setface-bold header-xmew
+(mew-setface header-xmew
   :tty "yellow" :light "chocolate" :dark"chocolate")
 
-(mew-setface-bold header-xmew-bad
+(mew-setface header-xmew-bad
   :tty "red" :light "red" :dark "red")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -49,14 +47,14 @@
 ;;; Highlight body
 ;;;
 
-(mew-setface body-url
+(mew-setface-bold body-url
   :tty "red" :light "Firebrick" :dark "OrangeRed")
 
 (mew-setface body-comment
   :tty "blue" :light "gray50" :dark "gray50")
 
 (mew-setface body-cite1
-  :tty "red" :light "ForestGreen" :dark "LimeGreen")
+  :tty "green" :light "ForestGreen" :dark "LimeGreen")
 
 (mew-setface body-cite2
   :tty "cyan" :light "MediumBlue" :dark "SkyBlue")
@@ -98,10 +96,10 @@
 ;;; Highlight eof
 ;;;
 
-(mew-setface-bold eof-message
+(mew-setface eof-message
   :tty "green" :light "ForestGreen" :dark "LimeGreen")
 
-(mew-setface-bold eof-part
+(mew-setface eof-part
   :tty "yellow" :light "DarkOrange4" :dark "Gold")
 
 ;;;; COPY TO HERE
