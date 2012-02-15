e09ed3e915ac89f5228e63bf979cd5d51bc0599e
Tomokazu Hirai <tomokazu.hirai@gmail.com>
2012-02-15 16:55:40 +0900

    added for backup oldfiles

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# backup old files
+mkdir $HOME/dotold
+mv -f $HOME/.emacs $HOME/.emacs.d $HOME/.mew-theme.el $HOME/.conkyrc \
+    $HOME/.Xresources $HOME/.twmrc $HOME/.zshrc $HOME/.vimrc \
+    $HOME/.vim $HOME/.screenrc $HOME/.xinitrc $HOME/.gtkrc-2.0 $HOME/.dir_colors \
+    $HOME/dotold/
+
 # download oh-my-zsh
 mv $HOME/.zshrc $HOME/.zshrc.orig
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -10,15 +17,14 @@ ln -s $HOME/dotfiles/.oh-my-zsh/custom/custom-env.zsh $HOME/.oh-my-zsh/custom/cu
 # download neobundle
 git clone git://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
 
+# symlink to $HOME
 ln -s $HOME/dotfiles/.emacs $HOME/.emacs
-#ln -s $HOME/dotfiles/.emacs.d $HOME/.emacs.d
-cp -Rp $HOME/dotfiles.emacs.d $HOME/.emacs.d
+cp -Rp $HOME/dotfiles/.emacs.d $HOME/.emacs.d
 ln -s $HOME/dotfiles/.mew-theme.el $HOME/.mew-theme.el
 ln -s $HOME/dotfiles/.conkyrc $HOME/.conkyrc
 ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
 ln -s $HOME/dotfiles/.twmrc $HOME/.twmrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-#ln -s $HOME/dotfiles/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
