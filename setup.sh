9726900e6927407a237ccb1fb58243d7b1de7af9
Tomokazu HIRAI <t.hirai@kidsdiary.jp>
2019-04-22 17:51:54 +0900

    zsh theme gruvbox を追加

--- setup.sh
+++ setup.sh
@@ -50,6 +50,7 @@ link .oh-my-zsh/custom/custom-env.zsh $HOME/.oh-my-zsh/custom/custom-env.zsh
 link .oh-my-zsh/custom/themes/jedipunkz.zsh-theme $HOME/.oh-my-zsh/custom/themes/jedipunkz.zsh-theme
 link .oh-my-zsh/custom/themes/jedipunkz-colorfull.zsh-theme $HOME/.oh-my-zsh/custom/themes/jedipunkz-colorfull.zsh-theme
 link .oh-my-zsh/custom/themes/jedipunkz-gruvbox.zsh-theme $HOME/.oh-my-zsh/custom/themes/jedipunkz-gruvbox.zsh-theme
+link .oh-my-zsh/custom/themes/gruvbox.zsh-theme $HOME/.oh-my-zsh/custom/themes/gruvbox.zsh-theme
 link .gtkrc-2.0 $HOME/.gtkrc-2.0
 link .emacs.d $HOME/.emacs.d
 link .Xresources $HOME/.Xresources
