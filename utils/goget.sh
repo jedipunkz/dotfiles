72dc7140017c1b668484597c93a2dda0e5f23b7d
Tomokazu Hirai <tomokazu.hirai@gmail.com>
2020-09-15 20:22:59 +0900

    remove goenv.sh and add comment to goget.sh

--- utils/goget.sh
+++ utils/goget.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-go get github.com/x-motemen/ghq
-go get golang.org/x/tools/cmd/goimports
-go get golang.org/x/tools/gopls
-go get github.com/nametake/golangci-lint-langserver
+cat << EOS
+gvm install goX.XX.XX
+gvm use goX.XX.XX --default
+gvm pkgenv global
+## Edit: export GOPRIVATE; GOPRIVATE="github.com/readyfor"
+gvm pkgset use global
+git config --global ghq.root "$GOPATH/src"
+bash ./goget.sh
+## boot vim and exec :LspInstallServer
+##
+## if you already did abobe operations, exec: bash dotfiles/utils/goget.sh -f # -f: force opts
+EOS
 
-# ToDo
-# .gvm/environments/go1.15.1@global を参考に GOPATH, GOPRIVATE を設定する必要がある
+if [ "$1" = "-f" ]; then
+  go get github.com/x-motemen/ghq
+  go get golang.org/x/tools/cmd/goimports
+  go get -u golang.org/x/lint/golint
+  go get golang.org/x/tools/gopls
+  go get github.com/nametake/golangci-lint-langserver
+fi
