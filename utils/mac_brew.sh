b78784ab45ac8fd2d9c363c2e081df4bea23ff38
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2021-10-15 22:25:54 +0900

    install colorful tools

--- utils/mac_brew.sh
+++ utils/mac_brew.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-brew install git tmux htop mercurial peco starship coreutils tfenv tree
+brew install \
+    git \
+    tmux \
+    htop \
+    mercurial \
+    peco \
+    starship \
+    coreutils \
+    tfenv \
+    tree \
+    exa \
+    dust \
+    duf \
+    broot \
+    dog
