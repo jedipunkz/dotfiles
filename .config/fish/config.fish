6506c568c993e5f915e1708c58b13645e3d609c9
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2021-11-23 14:35:56 +0900

    dracula theme for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,6 +59,10 @@ end
 set -x GOPATH $HOME/ghq
 set -x PATH $GOPATH/bin $PATH
 
+# fzf
+set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
+
 # starship.rs
 set -x STARSHIP_CONFIG ~/.starship
 set -x AWS_PROFILE default
