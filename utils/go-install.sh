b9ddc59b7a447499913fff1ed2ce76d839c88c26
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2022-12-22 21:41:16 +0900

    install gopls

--- utils/go-install.sh
+++ utils/go-install.sh
@@ -3,7 +3,7 @@
 go install github.com/x-motemen/ghq@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/lint/golint@latest
-# go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/gopls@latest
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/cweill/gotests/...@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
