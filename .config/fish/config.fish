f7e32908759f22bc330016b021e4c4f921e8e448
Tomokazu Hirai <tomokazu_hirai@EMP-P0063.local>
2023-06-25 20:18:37 +0900

    add path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ end
 set -U fish_greeting ""
 
 # PATH
-set -x PATH $HOME/.cargo/bin $HOME/.bin /usr/local/go/bin /usr/local/bin /usr/local/sbin /opt/homebrew/bin /bin /usr/bin /sbin /usr/sbin /usr/local/sessionmanagerplugin/bin $HOME/google-cloud-sdk/bin
+set -x PATH $HOME/.cargo/bin $HOME/.bin /usr/local/go/bin /usr/local/bin /usr/local/sbin /opt/homebrew/bin /bin /usr/bin /sbin /usr/sbin /usr/local/sessionmanagerplugin/bin $HOME/google-cloud-sdk/bin $HOME/.local/bin
 
 # Bind keys
 # bind \t forward-char
