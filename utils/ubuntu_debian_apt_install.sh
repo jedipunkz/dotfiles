b87ea626341d0fbe6d39df927f36f04b91dbc731
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2023-08-20 06:39:01 +0900

    add liblzma-dev

--- utils/ubuntu_debian_apt_install.sh
+++ utils/ubuntu_debian_apt_install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # pyenv
-sudo apt-get -y install gcc make libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libffi-dev tk-dev
+sudo apt-get -y install gcc make libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libffi-dev tk-dev liblzma-dev
 
 # rbenv
 sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev ruby-dev libxml2-dev libxslt-dev g++
