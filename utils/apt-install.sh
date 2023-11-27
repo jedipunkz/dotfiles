2d1e0916b1c1eaef4d9a65219d09598fd5f4420b
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2023-11-27 19:17:48 +0900

    remove comment-out

--- utils/apt-install.sh
+++ utils/apt-install.sh
@@ -9,14 +9,11 @@ sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev ru
 # basics
 sudo apt-get -y install zsh fish git tig tree tmux htop xclip peco bison fzf bat exa ripgrep fd-find luajit
 
-# 引数によってインストールするパッケージを変更する魔法の部分です！
 case "$1" in
   --with-i3)
-    # i3 関連のパッケージ
     sudo apt-get install -y xorg i3 alsa-utils polybar feh nemo fuse fcitx-mozc xcompmgr rofi mozc-utils-gui fcitx-config-gtk
     ;;
   --with-sway)
-    # sway 関連のパッケージ
     sudo apt-get install -y sway wdisplays fuse language-pack-ja nemo fonts-noto xdotool grim waybar fcitx5-frontend-gtk4 fcitx5-mozc wofi wl-clipboard mozc-utils-gui fcitx-config-gtk
     ;;
   *)
