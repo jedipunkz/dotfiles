be438106196dbfc400317deb8fd71f0f3b39ab36
Tomokazu HIRAI <thirai@xwing.local>
2024-03-16 11:12:36 +0900

    exa to eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,8 +32,8 @@ if test (uname -s) = "Linux"
 
 end
 
-alias ls="exa"
-alias lt='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+alias ls="eza"
+alias lt='eza -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 #alias vim="nvim"
 #alias vim="flatpak run io.neovim.nvim"
 alias la="ls -a"
