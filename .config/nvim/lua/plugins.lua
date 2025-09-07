f4e1c49e17df4bc9654e4967ae90cf1d78836f49
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2025-09-07 13:14:59 +0900

    chore: use DoomOne nvim theme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -45,7 +45,45 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Colorscheme
-  use 'morhetz/gruvbox'
+  -- use 'morhetz/gruvbox'
+  -- DoomeOne theme
+  use({
+    'NTBBloodbath/doom-one.nvim',
+    setup = function()
+        -- Add color to cursor
+        vim.g.doom_one_cursor_coloring = false
+        -- Set :terminal colors
+        vim.g.doom_one_terminal_colors = true
+        -- Enable italic comments
+        vim.g.doom_one_italic_comments = false
+        -- Enable TS support
+        vim.g.doom_one_enable_treesitter = true
+        -- Color whole diagnostic text or only underline
+        vim.g.doom_one_diagnostics_text_color = false
+        -- Enable transparent background
+        vim.g.doom_one_transparent_background = false
+
+        -- Pumblend transparency
+        vim.g.doom_one_pumblend_enable = false
+        vim.g.doom_one_pumblend_transparency = 20
+
+        -- Plugins integration
+        vim.g.doom_one_plugin_neorg = true
+        vim.g.doom_one_plugin_barbar = false
+        vim.g.doom_one_plugin_telescope = false
+        vim.g.doom_one_plugin_neogit = true
+        vim.g.doom_one_plugin_nvim_tree = true
+        vim.g.doom_one_plugin_dashboard = true
+        vim.g.doom_one_plugin_startify = true
+        vim.g.doom_one_plugin_whichkey = true
+        vim.g.doom_one_plugin_indent_blankline = true
+        vim.g.doom_one_plugin_vim_illuminate = true
+        vim.g.doom_one_plugin_lspsaga = false
+    end,
+    config = function()
+        vim.cmd("colorscheme doom-one")
+    end,
+  })
 
   -- Statusline
   use {
@@ -141,12 +179,12 @@ use({
           terminal = { enabled = true },
         })
       end
-      
+
       -- Create Snacks commands manually
       vim.api.nvim_create_user_command('SnackTerminal', function()
         require('snacks').terminal.toggle()
       end, { desc = 'Toggle terminal' })
-      
+
       vim.api.nvim_create_user_command('SnackTerminalOpen', function()
         require('snacks').terminal.open()
       end, { desc = 'Open terminal' })
