d57693ef906b3c82359ca1fa53fb8073a7a7eb93
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2025-12-13 19:57:28 +0900

    fix

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,84 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+        },
+      },
+      dashboard = {
+        enabled = true,
+        preset = {
+          header = [[
+░██████╗░██╗░░██╗░█████╗░░██████╗████████╗
+██╔════╝░██║░░██║██╔══██╗██╔════╝╚══██╔══╝
+██║░░██╗░███████║██║░░██║╚█████╗░░░░██║░░░
+██║░░╚██╗██╔══██║██║░░██║░╚═══██╗░░░██║░░░
+╚██████╔╝██║░░██║╚█████╔╝██████╔╝░░░██║░░░
+░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═════╝░░░░╚═╝░░░]],
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Recent Files",
+            section = "recent_files",
+            indent = 2,
+            padding = 1,
+          },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Projects",
+            section = "projects",
+            indent = 2,
+            padding = 1,
+          },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
+      terminal = {
+        enabled = true,
+        win = {
+          wo = {
+            winhl = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+        },
+      },
+      explorer = {
+        enabled = true,
+        hidden = true,
+      },
+      gitbrowse = {
+        enabled = true,
+      },
+      lazygit = {
+        enabled = true,
+        configure = true,
+        win = {
+          style = "lazygit",
+        },
+      },
+    },
     init = function()
       vim.api.nvim_create_autocmd("User", {
         pattern = "VeryLazy",
@@ -18,89 +96,6 @@ return {
           vim.keymap.set("n", "<C-e>", function() Snacks.explorer() end, { desc = "File explorer" })
         end,
       })
-    end,
-    config = function()
-      local snacks = require("snacks")
-      if not snacks.did_setup then
-        snacks.setup({
-          picker = {
-            sources = {
-              files = {
-                hidden = true,
-              },
-            },
-          },
-          dashboard = {
-            enabled = true,
-            sections = {
-              {
-                section = "header",
-                header = [[
-░░░░░██╗███████╗██████╗░██╗██████╗░██╗░░░██╗███╗░░██╗██╗░░██╗███████╗
-░░░░░██║██╔════╝██╔══██╗██║██╔══██╗██║░░░██║████╗░██║██║░██╔╝╚════██║
-░░░░░██║█████╗░░██║░░██║██║██████╔╝██║░░░██║██╔██╗██║█████═╝░░░███╔═╝
-██╗░░██║██╔══╝░░██║░░██║██║██╔═══╝░██║░░░██║██║╚████║██╔═██╗░██╔══╝░░
-╚█████╔╝███████╗██████╔╝██║██║░░░░░╚██████╔╝██║░╚███║██║░╚██╗███████╗
-░╚════╝░╚══════╝╚═════╝░╚═╝╚═╝░░░░░░╚═════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝]],
-              },
-              { section = "keys", gap = 1, padding = 1 },
-              {
-                pane = 2,
-                icon = " ",
-                title = "Recent Files",
-                section = "recent_files",
-                indent = 2,
-                padding = 1,
-              },
-              {
-                pane = 2,
-                icon = " ",
-                title = "Projects",
-                section = "projects",
-                indent = 2,
-                padding = 1,
-              },
-              {
-                pane = 2,
-                icon = " ",
-                title = "Git Status",
-                section = "terminal",
-                enabled = function()
-                  return Snacks.git.get_root() ~= nil
-                end,
-                cmd = "git status --short --branch --renames",
-                height = 5,
-                padding = 1,
-                ttl = 5 * 60,
-                indent = 3,
-              },
-              { section = "startup" },
-            },
-          },
-          terminal = {
-            enabled = true,
-            win = {
-              wo = {
-                winhl = "Normal:Normal,FloatBorder:FloatBorder",
-              },
-            },
-          },
-          explorer = {
-            enabled = true,
-            hidden = true,
-          },
-          gitbrowse = {
-            enabled = true,
-          },
-          lazygit = {
-            enabled = true,
-            configure = true,
-            win = {
-              style = "lazygit",
-            },
-          },
-        })
-      end
     end,
   },
 
