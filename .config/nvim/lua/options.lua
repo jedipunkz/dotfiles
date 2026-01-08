1ae6b3b494ed9f09771f375e3e1c2b1c73dfdf95
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-01-08 13:18:37 +0900

    chore: enable mouse on nvim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -40,7 +40,7 @@ local options = {
 	guifont = "monospace:h17",
 	splitbelow = false, -- オンのとき、ウィンドウを横分割すると新しいウィンドウはカレントウィンドウの下に開かれる
 	splitright = false, -- オンのとき、ウィンドウを縦分割すると新しいウィンドウはカレントウィンドウの右に開かれる
-  mouse = "", 
+  mouse = "a",  -- Enable mouse support (required for scrolling in terminal apps) 
 }
 
 vim.opt.shortmess:append("c")
