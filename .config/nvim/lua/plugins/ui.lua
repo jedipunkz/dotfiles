4656ffe59c7d4f87244bf8174fefc48a6b196c65
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-01-31 15:45:23 +0900

    nvim: update color theme

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -108,7 +108,7 @@ return {
       lualine.setup {
         options = {
           icons_enabled = true,
-          theme = 'dracula',
+          theme = 'tokyonight',
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
           disabled_filetypes = {
