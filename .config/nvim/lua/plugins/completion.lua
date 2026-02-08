7a9e10d085a9f318d15b1aa45ed23de5631cb388
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-02-08 13:21:28 +0900

    nvim: enable blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,119 +1,70 @@
 return {
-  -- Completions
   {
-    "hrsh7th/nvim-cmp",
+    "saghen/blink.cmp",
+    version = "1.*",
     dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "onsails/lspkind-nvim",
       "hrsh7th/vim-vsnip",
+      "fang2hou/blink-copilot",
+      {
+        "zbirenbaum/copilot.lua",
+        config = function()
+          require("copilot").setup({
+            suggestion = {
+              enabled = true,
+              auto_trigger = true,
+              keymap = {
+                accept = "<Tab>",
+                next = "<C-n>",
+                prev = "<C-p>",
+                dismiss = "<C-e>",
+              },
+            },
+            panel = { enabled = false },
+          })
+        end,
+      },
     },
     config = function()
-      local cmp = require('cmp')
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+      require("blink.cmp").setup({
+        keymap = {
+          preset = 'none',
+          ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+          ['<C-e>'] = { 'cancel', 'fallback' },
+          ['<CR>'] = { 'accept', 'fallback' },
+          ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+          ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+          ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+          ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        },
+        completion = {
+          documentation = { auto_show = true, auto_show_delay_ms = 200 },
+          ghost_text = { enabled = false },  -- NES との競合回避
+        },
+        snippets = { preset = 'vsnip' },
+        sources = {
+          default = { 'copilot', 'lsp', 'snippets', 'path', 'buffer' },
+          providers = {
+            copilot = {
+              name = "copilot",
+              module = "blink-copilot",
+              async = true,
+              score_offset = 100,
+              opts = { max_completions = 3, debounce = 200 },
+            },
+          },
+        },
+        cmdline = {
+          enabled = true,
+          sources = function()
+            local t = vim.fn.getcmdtype()
+            if t == '/' or t == '?' then return { 'buffer' } end
+            if t == ':' then return { 'path', 'cmdline' } end
+            return {}
           end,
         },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            -- Copilot NES が有効な場合はそちらを優先
-            local bufnr = vim.api.nvim_get_current_buf()
-            local nes_state = vim.b[bufnr].nes_state
-            if nes_state then
-              local copilot_nes = require("copilot-lsp.nes")
-              local _ = copilot_nes.walk_cursor_start_edit()
-                or (copilot_nes.apply_pending_nes() and copilot_nes.walk_cursor_end_edit())
-              return
-            end
-            -- cmp メニューが表示されている場合は次の候補を選択
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-
-      -- Git commit completion
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'cmp_git' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-
-      -- Command line completion for / and ?
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
-
-      -- Command line completion for :
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
+        signature = { enabled = true },
       })
     end,
   },
-  {
-    "hrsh7th/cmp-buffer",
-  },
-  {
-    "hrsh7th/cmp-path",
-  },
-  {
-    "hrsh7th/cmp-cmdline",
-  },
-  {
-    "saadparwaiz1/cmp_luasnip",
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "hrsh7th/cmp-nvim-lua",
-  },
-  {
-    "onsails/lspkind-nvim",
-  },
-  {
-    "hrsh7th/vim-vsnip",
-  },
+  { "hrsh7th/vim-vsnip" },
 }
