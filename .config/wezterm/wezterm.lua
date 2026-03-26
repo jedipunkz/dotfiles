45c03276095e309061e2a787f5fcb13e7beaf4b6
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-03-26 21:23:14 +0900

    wezterm: font size update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,11 @@ if wezterm.target_triple:find("linux") then
   font_size = 12 -- Linux用のフォントサイズ
   window_background_opacity = 0.8 -- 透明化
 elseif wezterm.target_triple:find("darwin") then
-  font_size = 14 -- macOS用のフォントサイズ
+  if wezterm.hostname() == "EMP-P0063.local" then
+    font_size = 15
+  else
+    font_size = 14
+  end
   window_background_opacity = 1.0 -- 透明化せず
 end
 
