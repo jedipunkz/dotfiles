ebc45a351831a127ebf16bc2629fc49214972bb5
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-05-24 12:40:45 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,11 @@ if test (uname -s) = "Darwin"
     set -x EDITOR nvim
     alias vim="nvim"
 else
-    alias cat="batcat"
+    if type -q bat
+        alias cat="bat"
+    else if type -q batcat
+        alias cat="batcat"
+    end
     alias vim="nvim"
     alias code="/mnt/c/Users/tomok/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
 end
