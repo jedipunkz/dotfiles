2922915808d644b292551277b8e08fc974445065
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2026-09-20 11:47:17 +0900

    fix

--- utils/apt-install.sh
+++ utils/apt-install.sh
@@ -3,9 +3,9 @@
 sudo apt-get update
 
 # pyenv
-sudo apt-get -y install gcc make libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libffi-dev tk-dev liblzma-dev
+# sudo apt-get -y install gcc make libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libffi-dev tk-dev liblzma-dev
 # rbenv
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev ruby-dev libxml2-dev libxslt-dev g++
+# sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev ruby-dev libxml2-dev libxslt-dev g++
 # basics
 sudo apt-get -y install zsh fish git tig tree tmux htop xclip peco bison fzf bat eza ripgrep fd-find luajit
 
